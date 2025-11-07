997bf0430eb08385eb9efedf6df607f6f6b25877
RINCHEN <12220058.gcit@rub.edu.bt>
2025-11-07 09:56:39 +0600

    first commit

---
+++ src/constants.js
@@ -0,0 +1,298 @@
+// Skills Section Logo's
+import htmlLogo from "./assets/tech_logo/html.png";
+import cssLogo from "./assets/tech_logo/css.png";
+import sassLogo from "./assets/tech_logo/sass.png";
+import javascriptLogo from "./assets/tech_logo/javascript.png";
+import reactjsLogo from "./assets/tech_logo/reactjs.png";
+import angularLogo from "./assets/tech_logo/angular.png";
+import reduxLogo from "./assets/tech_logo/redux.png";
+import nextjsLogo from "./assets/tech_logo/nextjs.png";
+import tailwindcssLogo from "./assets/tech_logo/tailwindcss.png";
+import gsapLogo from "./assets/tech_logo/gsap.png";
+import materialuiLogo from "./assets/tech_logo/materialui.png";
+import bootstrapLogo from "./assets/tech_logo/bootstrap.png";
+import springbootLogo from "./assets/tech_logo/springboot.png";
+import nodejsLogo from "./assets/tech_logo/nodejs.png";
+import expressjsLogo from "./assets/tech_logo/express.png";
+import mysqlLogo from "./assets/tech_logo/mysql.png";
+import mongodbLogo from "./assets/tech_logo/mongodb.png";
+import firebaseLogo from "./assets/tech_logo/firebase.png";
+import cLogo from "./assets/tech_logo/c.png";
+import cppLogo from "./assets/tech_logo/cpp.png";
+import javaLogo from "./assets/tech_logo/java.png";
+import pythonLogo from "./assets/tech_logo/python.png";
+import typescriptLogo from "./assets/tech_logo/typescript.png";
+import gitLogo from "./assets/tech_logo/git.png";
+import githubLogo from "./assets/tech_logo/github.png";
+import vscodeLogo from "./assets/tech_logo/vscode.png";
+import postmanLogo from "./assets/tech_logo/postman.png";
+import mcLogo from "./assets/tech_logo/mc.png";
+import figmaLogo from "./assets/tech_logo/figma.png";
+import netlifyLogo from "./assets/tech_logo/netlify.png";
+import vercelLogo from "./assets/tech_logo/vercel.png";
+import postgreLogo from "./assets/tech_logo/postgre.png";
+import csharpLogo from "./assets/tech_logo/csharp.png";
+
+// Experience Section Logo's
+import nhdclLogo from "./assets/company_logo/nhdcl.png";
+// import agcLogo from "./assets/company_logo/agc_logo.png";
+// import newtonschoolLogo from "./assets/company_logo/newtonschool_logo.png";
+
+// Education Section Logo's
+import gcitLogo from "./assets/education_logo/gcit.png";
+import nangkorLogo from "./assets/education_logo/nangkor.png";
+import mcsLogo from "./assets/education_logo/mcs.png";
+
+// Project Section Logo's
+import drukcraftLogo from "./assets/work_logo/drukcraft.png";
+import yoloLogo from "./assets/work_logo/yolo.png";
+import movierecLogo from "./assets/work_logo/nhdcl.png";
+import taskremLogo from "./assets/work_logo/task_rem.png";
+import npmLogo from "./assets/work_logo/npm.png";
+import webverLogo from "./assets/work_logo/web_dig.png";
+import cmLogo from "./assets/work_logo/cm.png";
+import imagesearchLogo from "./assets/work_logo/image_search.png";
+import removebgLogo from "./assets/work_logo/remove_bg.png";
+
+export const SkillsInfo = [
+  {
+    title: "Frontend",
+    skills: [
+      { name: "HTML", logo: htmlLogo },
+      { name: "CSS", logo: cssLogo },
+      // { name: "SASS", logo: sassLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+      { name: "React JS", logo: reactjsLogo },
+      // { name: "Angular", logo: angularLogo },
+      // { name: "Redux", logo: reduxLogo },
+      { name: "Next JS", logo: nextjsLogo },
+      { name: "Tailwind CSS", logo: tailwindcssLogo },
+      // { name: "GSAP", logo: gsapLogo },
+      { name: "Material UI", logo: materialuiLogo },
+      { name: "Bootstrap", logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      { name: "Springboot", logo: springbootLogo },
+      { name: "Node JS", logo: nodejsLogo },
+      { name: "Express JS", logo: expressjsLogo },
+      { name: "MySQL", logo: mysqlLogo },
+      { name: "MongoDB", logo: mongodbLogo },
+      // { name: "Firebase", logo: firebaseLogo },
+      { name: "PostgreSQL", logo: postgreLogo },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      // { name: "C", logo: cLogo },
+      // { name: "C++", logo: cppLogo },
+      { name: "Java", logo: javaLogo },
+      { name: "Python", logo: pythonLogo },
+      // { name: "C-Sharp", logo: csharpLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+      { name: "TypeScript", logo: typescriptLogo },
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      { name: "Git", logo: gitLogo },
+      { name: "GitHub", logo: githubLogo },
+      { name: "VS Code", logo: vscodeLogo },
+      { name: "Postman", logo: postmanLogo },
+      { name: "Compass", logo: mcLogo },
+      { name: "Vercel", logo: vercelLogo },
+      // { name: "Netlify", logo: netlifyLogo },
+      { name: "Figma", logo: figmaLogo },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: nhdclLogo,
+    role: "Fullstack Developer",
+    company: "NHDCL",
+    date: "April 2025 - Present",
+    desc: "Developed dynamic and scalable web applications using the Microservice architecture, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React JS",
+      "Tailwind CSS",
+      "MongoDb",
+      "Spring Boot",
+    ],
+  },
+  // {
+  //   id: 1,
+  //   img: agcLogo,
+  //   role: "Fullstack Engineer",
+  //   company: "Agumentik Group of Companies",
+  //   date: "July 2023 - March 2024",
+  //   desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+  //   skills: [
+  //     "ReactJS",
+  //     "Redux",
+  //     "JavaScript",
+  //     "Tailwind CSS",
+  //     "HTML",
+  //     "CSS",
+  //     "SQL",
+  //   ],
+  // },
+  // {
+  //   id: 2,
+  //   img: newtonschoolLogo,
+  //   role: "Frontend Intern",
+  //   company: "Newton School",
+  //   date: "September 2021 - August 2022",
+  //   desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+  //   skills: ["HTML", "CSS", "Javascript", "Bootstrap", "Figma", "Material UI"],
+  // },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: gcitLogo,
+    school: "Gyalpozhing College of Information Technology, Thimphu",
+    date: " 2022 - Present",
+    grade: "_ CGPA",
+    desc: "I am currently pursuing a Bachelor of Computer Science (Full Stack Development) at Gyalpozhing College of Information Technology (GCIT), Thimphu. Throughout my studies, I have gained strong skills in both front-end and back-end development, including HTML, CSS, JavaScript, React, Node.js, Express, and MongoDB. I have also worked with databases, learned software engineering principles, and built projects that integrate the full web development stack. Actively participating in workshops and technical events has helped me apply my knowledge in real-world scenarios and strengthened my problem-solving abilities. My education at GCIT has equipped me with the practical skills and confidence to develop complete web applications from start to finish.",
+    degree: "Bachelor of Computer Science - BCS",
+  },
+  {
+    id: 1,
+    img: nangkorLogo,
+    school: "Nangkor Central School, Pemagatshel",
+    date: "2018 - 2021",
+    grade: "73.2%",
+    desc: "I completed my Higher Secondary School education from Nangkor Central School, Pemagatshel. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at Nangkor Central School allowed me to work on projects that applied theoretical concepts to real-world problems.",
+    degree: "Higher Secondary School(XII), Science",
+  },
+  {
+    id: 2,
+    img: mcsLogo,
+    school: "Minjiwoong Central School, Samdrup Jongkhar",
+    date: "2014 - 2017",
+    grade: "78%",
+    desc: "I completed my middle school education from Minjiwoong Central School, Samdrup Jongkhar, where I studied Physics, Chemistry, and Mathematics with Economics.",
+    degree: "Middle School(X)",
+  },
+  // {
+  //   id: 3,
+  //   img: mcsLogo,
+  //   school: "Vatsalya Public School Govardhan, Mathura",
+  //   date: "Apr 2015 - March 2016",
+  //   grade: "87.5%",
+  //   desc: "I completed my class 10 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Science with Computer.",
+  //   degree: "CBSE(X), Science with Computer Application",
+  // },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "DrukCraft",
+    description:
+      "It's a eCommerce website where user can buy both national and international products.",
+    image: drukcraftLogo,
+    tags: ["HTML", "CSS", "JavaScript", "Typescript", "API"],
+    github: "https://github.com/RinchenDrukcraft/DrukCraftFrontend.git",
+    // webapp: "https://githubprofiledetective.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "Yolo Bhutan",
+    description:
+      "A full-stack eCommerce platform connecting local artisans with global customers.",
+    image: yoloLogo,
+    tags: [
+      "React JS",
+      "Node.js",
+      "MongoDB",
+      "TypeScript",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    github: "https://github.com/Anup-gurung/yoloFrontend.git",
+    // webapp: "https://csprep.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Gyalsung Academy web Application",
+    description:
+      "A React-based web application that management Gyalsung Academy of Bhutan. The intuitive design and smooth experience make it a go-to web for Gyalsung academy management.",
+    image: movierecLogo,
+    tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+    // github: "https://github.com/codingmastr/Movie-Recommendation-App",
+    webapp: "https://nhdcl-frontend.onrender.com/",
+  },
+  // {
+  //   id: 3,
+  //   title: "Email Validator NPM Package",
+  //   description:
+  //     "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
+  //   image: npmLogo,
+  //   tags: ["React JS", "Node.js", "NPM", "Validation"],
+  //   github: "https://github.com/codingmastr/cmtk-email-validator",
+  //   webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Task Reminder Chrome Extension Tool",
+  //   description:
+  //     "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+  //   image: taskremLogo,
+  //   tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+  //   github: "https://github.com/codingmastr/Task-Reminder-Tool",
+  //   webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+  // },
+  // {
+  //   id: 5,
+  //   title: "Webverse Digital",
+  //   description:
+  //     "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
+  //   image: webverLogo,
+  //   tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+  //   github: "https://github.com/codingmastr/Webverse-Digital",
+  //   webapp: "https://webversedigital.com/",
+  // },
+  // {
+  //   id: 6,
+  //   title: "Coding Master",
+  //   description:
+  //     "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+  //   image: cmLogo,
+  //   tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+  //   github: "https://codingmasterweb.in/",
+  //   webapp: "https://codingmasterweb.in/",
+  // },
+  // {
+  //   id: 7,
+  //   title: "Image Search App",
+  //   description:
+  //     "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+  //   image: imagesearchLogo,
+  //   tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+  //   github: "https://github.com/codingmastr/Image-Search-App",
+  //   webapp: "https://imagsearch.netlify.app/",
+  // },
+  // {
+  //   id: 8,
+  //   title: "Image Background Remover",
+  //   description:
+  //     "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+  //   image: removebgLogo,
+  //   tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+  //   github: "https://github.com/codingmastr/Image-Background-Remover",
+  //   webapp: "https://removeyourbg.netlify.app/",
+  // },
+];
